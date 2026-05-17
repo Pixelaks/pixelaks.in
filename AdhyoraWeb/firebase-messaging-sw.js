1ff38edd73ca0d81e7722161a2b95d5a28113e92
ANAGH K SATHEESH <86482414+anaghksatheesh@users.noreply.github.com>
2026-05-17 19:36:52 +0530

    Update firebase-messaging-sw.js

--- AdhyoraWeb/firebase-messaging-sw.js
+++ AdhyoraWeb/firebase-messaging-sw.js
@@ -76,10 +76,3 @@ self.addEventListener('install', (event) => {
 self.addEventListener('activate', (event) => {
     event.waitUntil(clients.claim());
 });
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        fetch(event.request, { cache: "no-store" })
-            .catch(() => caches.match(event.request))
-    );
-});
