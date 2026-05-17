0483f9c3f5ebfc44fcae04f855b78eca24384a70
ANAGH K SATHEESH <86482414+anaghksatheesh@users.noreply.github.com>
2026-05-17 22:44:09 +0530

    Update version-check.js

--- AdhyoraWeb/version-check.js
+++ AdhyoraWeb/version-check.js
@@ -20,7 +20,7 @@ remoteConfig.settings = {
 // ==========================================
 // 🚨 CHANGE ONLY THIS VERSION
 // ==========================================
-const LOCAL_VERSION = "1.0.1";
+const LOCAL_VERSION = "1.0.2";
 
 // ==========================================
 // SHOW VERSION TEXT
